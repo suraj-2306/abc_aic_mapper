d0dc4f5a1dd3185cf60284a48d9b772d5240dc58
suraj <suraj@shubham-rai.zih.tu-dresden.de>
2022-09-05 10:38:49 +0200

    Fixed the architecture for the mapper

--- src/map/cm/cmBalance.c
+++ src/map/cm/cmBalance.c
@@ -1,23 +1,22 @@
-  /**CFile****************************************************************
+/**CFile****************************************************************
 
-  FileName    [cmArea.c]
+FileName    [cmArea.c]
 
-  SystemName  [ABC: Logic synthesis and verification system.]
+SystemName  [ABC: Logic synthesis and verification system.]
 
-  PackageName [Cone mapping based on priority cuts.]
+PackageName [Cone mapping based on priority cuts.]
 
-  Synopsis    [Functions balance cuts.]
+Synopsis    [Functions balance cuts.]
 
-  Author      [Martin Thuemmler]
-  
-  Affiliation [TU Dresden]
+Author      [Martin Thuemmler]
 
-  Date        [Ver. 1.0. Started - February 15, 2021.]
+Affiliation [TU Dresden]
 
-  Revision    [$Id: cmArea.c,v 1.00 2021/02/15 00:00:00 thm Exp $]
+Date        [Ver. 1.0. Started - February 15, 2021.]
+
+Revision    [$Id: cmArea.c,v 1.00 2021/02/15 00:00:00 thm Exp $]
 
 ***********************************************************************/
-
 
 #include "cm.h"
 
@@ -40,12 +39,13 @@ ABC_NAMESPACE_IMPL_START
 
 ***********************************************************************/
 
-Hop_Obj_t * Cm_ManCreateCutHop_rec(Hop_Man_t * pHm, Cm_Obj_t * pObj)
-{
-    if( (pObj->fMark & CM_MARK_LEAF_CUT) )
-        return Hop_IthVar(pHm, pObj->iTemp);
-    return Hop_And(pHm, Hop_NotCond(Cm_ManCreateCutHop_rec(pHm, pObj->pFanin0), pObj->fCompl0),
-                        Hop_NotCond(Cm_ManCreateCutHop_rec(pHm, pObj->pFanin1), pObj->fCompl1));
+Hop_Obj_t *Cm_ManCreateCutHop_rec(Hop_Man_t *pHm, Cm_Obj_t *pObj) {
+  if ((pObj->fMark & CM_MARK_LEAF_CUT))
+    return Hop_IthVar(pHm, pObj->iTemp);
+  return Hop_And(
+      pHm,
+      Hop_NotCond(Cm_ManCreateCutHop_rec(pHm, pObj->pFanin0), pObj->fCompl0),
+      Hop_NotCond(Cm_ManCreateCutHop_rec(pHm, pObj->pFanin1), pObj->fCompl1));
 }
 
 /**Function*************************************************************
@@ -59,21 +59,20 @@ Hop_Obj_t * Cm_ManCreateCutHop_rec(Hop_Man_t * pHm, Cm_Obj_t * pObj)
   SeeAlso     []
 
 ***********************************************************************/
-Cm_Obj_t * Cm_ManCreateEqCut_rec(Cm_Man_t *p, Hop_Obj_t * pHopObj, Cm_Obj_t * pCmRoot )
-{
-    if ( Hop_ObjIsPi(pHopObj) )
-    {
-        Cm_Obj_t * pL = pCmRoot->BestCut.Leafs[pHopObj->PioNum];
-        pL->fMark |= CM_MARK_SEEN;
-        return pL;
-    }
-    Hop_Obj_t *pH0 = Hop_ObjFanin0(pHopObj);
-    Hop_Obj_t *pH1 = Hop_ObjFanin1(pHopObj);
-    Cm_Obj_t * pRes = Cm_ManCreateAndEq(p, Cm_ManCreateEqCut_rec(p, pH0, pCmRoot),
-                                           Cm_ManCreateEqCut_rec(p, pH1, pCmRoot));
-    pRes->fCompl0 = Hop_ObjFaninC0(pHopObj);
-    pRes->fCompl1 = Hop_ObjFaninC1(pHopObj);
-    return pRes;
+Cm_Obj_t *Cm_ManCreateEqCut_rec(Cm_Man_t *p, Hop_Obj_t *pHopObj,
+                                Cm_Obj_t *pCmRoot) {
+  if (Hop_ObjIsPi(pHopObj)) {
+    Cm_Obj_t *pL = pCmRoot->BestCut.Leafs[pHopObj->PioNum];
+    pL->fMark |= CM_MARK_SEEN;
+    return pL;
+  }
+  Hop_Obj_t *pH0 = Hop_ObjFanin0(pHopObj);
+  Hop_Obj_t *pH1 = Hop_ObjFanin1(pHopObj);
+  Cm_Obj_t *pRes = Cm_ManCreateAndEq(p, Cm_ManCreateEqCut_rec(p, pH0, pCmRoot),
+                                     Cm_ManCreateEqCut_rec(p, pH1, pCmRoot));
+  pRes->fCompl0 = Hop_ObjFaninC0(pHopObj);
+  pRes->fCompl1 = Hop_ObjFaninC1(pHopObj);
+  return pRes;
 }
 
 /**Function*************************************************************
@@ -88,54 +87,51 @@ Cm_Obj_t * Cm_ManCreateEqCut_rec(Cm_Man_t *p, Hop_Obj_t * pHopObj, Cm_Obj_t * pC
   SeeAlso     []
 
 ***********************************************************************/
-Cm_Obj_t * Cm_ManBalanceCut(Cm_Man_t *p, Cm_Obj_t * pObj)
-{
-    Cm_Obj_t * pEqCut = NULL;
-    int d = pObj->BestCut.Depth;
-    for(int i=0; i<pObj->BestCut.nFanins; i++)
-    {
-        pObj->BestCut.Leafs[i]->iTemp = i;
-        pObj->BestCut.Leafs[i]->fMark |= CM_MARK_LEAF_CUT;
-        pObj->BestCut.Leafs[i]->fMark &= ~CM_MARK_SEEN;
-    }
-    Hop_Man_t * pHm = Hop_ManStart();
-    Hop_IthVar( pHm, pObj->BestCut.nFanins- 1 );
-    Hop_Obj_t *pHopRoot = Cm_ManCreateCutHop_rec(pHm, pObj);
-    Hop_ObjCreatePo(pHm, pHopRoot );
-    for(int i=0; i<pObj->BestCut.nFanins; i++) 
-        pObj->BestCut.Leafs[i]->fMark &= ~CM_MARK_LEAF_CUT;
-    Hop_Man_t *pN = Hop_ManBalance(pHm, 1);
-    int balancedDepth = Hop_ManCountLevels(pN);
-    Hop_ManStop(pHm);
-    if ( balancedDepth < d && d >= 2 && balancedDepth > 0)
-    {
-        Cm_PrintBestCut(pObj);
-        pHopRoot = Hop_ObjFanin0(Hop_ManPo(pN, 0));
-        // get last element in equivalence list
-        Cm_Obj_t * pPre = pObj;
-        while( pPre->pEquiv )
-            pPre = pPre->pEquiv;
-        
-        Cm_Obj_t * pEq = Cm_ManCreateEqCut_rec(p, pHopRoot, pObj);
+Cm_Obj_t *Cm_ManBalanceCut(Cm_Man_t *p, Cm_Obj_t *pObj) {
+  Cm_Obj_t *pEqCut = NULL;
+  int d = pObj->BestCut.Depth;
+  for (int i = 0; i < pObj->BestCut.nFanins; i++) {
+    pObj->BestCut.Leafs[i]->iTemp = i;
+    pObj->BestCut.Leafs[i]->fMark |= CM_MARK_LEAF_CUT;
+    pObj->BestCut.Leafs[i]->fMark &= ~CM_MARK_SEEN;
+  }
+  Hop_Man_t *pHm = Hop_ManStart();
+  Hop_IthVar(pHm, pObj->BestCut.nFanins - 1);
+  Hop_Obj_t *pHopRoot = Cm_ManCreateCutHop_rec(pHm, pObj);
+  Hop_ObjCreatePo(pHm, pHopRoot);
+  for (int i = 0; i < pObj->BestCut.nFanins; i++)
+    pObj->BestCut.Leafs[i]->fMark &= ~CM_MARK_LEAF_CUT;
+  Hop_Man_t *pN = Hop_ManBalance(pHm, 1);
+  int balancedDepth = Hop_ManCountLevels(pN);
+  Hop_ManStop(pHm);
+  if (balancedDepth < d && d >= 2 && balancedDepth > 0) {
+    // Cm_PrintBestCut(pObj);
+    pHopRoot = Hop_ObjFanin0(Hop_ManPo(pN, 0));
+    // get last element in equivalence list
+    Cm_Obj_t *pPre = pObj;
+    while (pPre->pEquiv)
+      pPre = pPre->pEquiv;
 
-        // transfer only used leafs
-        pEq->BestCut.nFanins = 0;
-        for(int i=0; i<pObj->BestCut.nFanins; i++)
-            if ( (pObj->BestCut.Leafs[i]->fMark & CM_MARK_SEEN) )
-                pEq->BestCut.Leafs[pEq->BestCut.nFanins++] = pObj->BestCut.Leafs[i];
-        pEq->BestCut.Depth = balancedDepth;
-        pEq->BestCut.SoOfCutAt = NULL;
-        Cm_Obj_t *pNodes[128];
-        pNodes[1] = pEq;
-        Cm_FaBuildWithMaximumDepth(pNodes, balancedDepth);
-        Cm_PrintFa(pNodes, balancedDepth); 
-        Cm_PrintBestCut(pEq);
-        // add node
-        pPre->pEquiv = pEq;
-        pEqCut = pEq;
-    }
-    Hop_ManStop(pN);
-    return pEqCut;
+    Cm_Obj_t *pEq = Cm_ManCreateEqCut_rec(p, pHopRoot, pObj);
+
+    // transfer only used leafs
+    pEq->BestCut.nFanins = 0;
+    for (int i = 0; i < pObj->BestCut.nFanins; i++)
+      if ((pObj->BestCut.Leafs[i]->fMark & CM_MARK_SEEN))
+        pEq->BestCut.Leafs[pEq->BestCut.nFanins++] = pObj->BestCut.Leafs[i];
+    pEq->BestCut.Depth = balancedDepth;
+    pEq->BestCut.SoOfCutAt = NULL;
+    Cm_Obj_t *pNodes[128];
+    pNodes[1] = pEq;
+    Cm_FaBuildWithMaximumDepth(pNodes, balancedDepth);
+    // Cm_PrintFa(pNodes, balancedDepth);
+    // Cm_PrintBestCut(pEq);
+    // add node
+    pPre->pEquiv = pEq;
+    pEqCut = pEq;
+  }
+  Hop_ManStop(pN);
+  return pEqCut;
 }
 
 ABC_NAMESPACE_IMPL_END
--- src/map/mimo/miMoUtil.c
+++ src/map/mimo/miMoUtil.c
@@ -9,7 +9,7 @@
   Synopsis    [Utility functionality]
 
   Author      [Martin Thuemmler]
-  
+
   Affiliation [TU Dresden]
 
   Date        [Ver. 1.0. Started - February 15, 2021.]
@@ -25,66 +25,59 @@ ABC_NAMESPACE_IMPL_START
   Synopsis    [Finds output pin of gate by name]
 
   Description []
-               
+
   SideEffects []
 
   SeeAlso     []
 
 ***********************************************************************/
-MiMo_PinOut_t * MiMo_GateFindPinOut(MiMo_Gate_t *pGate, char *pName)
-{
-    // for now slow check, that name is not already defined
-    int i; 
-    MiMo_PinOut_t * pPinOut;
-    MiMo_GateForEachPinOut(pGate, pPinOut, i)
-        if( !strcmp(pPinOut->pName, pName) )
-            return pPinOut;
-    return NULL;
-
+MiMo_PinOut_t *MiMo_GateFindPinOut(MiMo_Gate_t *pGate, char *pName) {
+  // for now slow check, that name is not already defined
+  int i;
+  MiMo_PinOut_t *pPinOut;
+  MiMo_GateForEachPinOut(pGate, pPinOut,
+                         i) if (!strcmp(pPinOut->pName, pName)) return pPinOut;
+  return NULL;
 }
- 
 
 /**Function*************************************************************
 
   Synopsis    [finds input pin of gate by given name]
 
   Description []
-               
+
   SideEffects []
 
   SeeAlso     []
 
 ***********************************************************************/
-MiMo_PinIn_t * MiMo_GateFindPinIn(MiMo_Gate_t *pGate, char *pName)
-{
-    // for now slow check, that name is not already defined
-    int i; 
-    MiMo_PinIn_t * pPinIn;
-    MiMo_GateForEachPinIn(pGate, pPinIn, i)
-        if( !strcmp(pPinIn->pName, pName) )
-            return pPinIn;
-    return NULL;
+MiMo_PinIn_t *MiMo_GateFindPinIn(MiMo_Gate_t *pGate, char *pName) {
+  // for now slow check, that name is not already defined
+  int i;
+  MiMo_PinIn_t *pPinIn;
+  MiMo_GateForEachPinIn(pGate, pPinIn,
+                        i) if (!strcmp(pPinIn->pName, pName)) return pPinIn;
+  return NULL;
 }
-
 
 /**Function*************************************************************
 
   Synopsis    [Sets delay of all delayList entries until given boundary]
 
   Description []
-               
+
   SideEffects []
 
   SeeAlso     []
 
 ***********************************************************************/
-void MiMo_DelayListSetDelay(MiMo_PinOut_t *pPinOut, MiMo_PinDelay_t * pLast, float delay){
-    MiMo_PinDelay_t * pDelayList = pPinOut->pDelayList;
-    while ( pDelayList && pDelayList != pLast )
-    {
-        pDelayList->Delay = delay;
-        pDelayList = pDelayList->pNext;
-    }
+void MiMo_DelayListSetDelay(MiMo_PinOut_t *pPinOut, MiMo_PinDelay_t *pLast,
+                            float delay) {
+  MiMo_PinDelay_t *pDelayList = pPinOut->pDelayList;
+  while (pDelayList && pDelayList != pLast) {
+    pDelayList->Delay = delay;
+    pDelayList = pDelayList->pNext;
+  }
 }
 
 /**Function*************************************************************
@@ -92,36 +85,36 @@ void MiMo_DelayListSetDelay(MiMo_PinOut_t *pPinOut, MiMo_PinDelay_t * pLast, flo
   Synopsis    [Creates a new delay list entry based on input pin string]
 
   Description []
-               
+
   SideEffects []
 
   SeeAlso     []
 
 ***********************************************************************/
-int MiMo_DelayListAdd(MiMo_Gate_t *pGate, MiMo_PinOut_t * pToPinOut, char *pFromPinStr)
-{
-    MiMo_PinIn_t * pPinIn = MiMo_GateFindPinIn(pGate, pFromPinStr);
-    if ( pPinIn )
-    {
-        MiMo_PinDelay_t * pPinDelay = ABC_ALLOC(MiMo_PinDelay_t, 1);
-        pPinDelay->fFromPinOut = 0;
-        pPinDelay->pFromPin = pPinIn;
-        pPinDelay->pNext = pToPinOut->pDelayList;
-        pToPinOut->pDelayList = pPinDelay;
-        return 1;
-    }
-    MiMo_PinOut_t * pPinOut =  MiMo_GateFindPinOut(pGate, pFromPinStr);
-    if ( pPinOut )
-    {
-        MiMo_PinDelay_t * pPinDelay = ABC_ALLOC(MiMo_PinDelay_t, 1);
-        pPinDelay->fFromPinOut = 1;
-        pPinDelay->pFromPin = pPinOut;
-        pPinDelay->pNext = pToPinOut->pDelayList;
-        pToPinOut->pDelayList = pPinDelay;
-        return 1;
-    }
-    printf("No Input or output pin %s for delay specification not found in gate %s\n", pFromPinStr, pGate->pName);
-    return 0;
+int MiMo_DelayListAdd(MiMo_Gate_t *pGate, MiMo_PinOut_t *pToPinOut,
+                      char *pFromPinStr) {
+  MiMo_PinIn_t *pPinIn = MiMo_GateFindPinIn(pGate, pFromPinStr);
+  if (pPinIn) {
+    MiMo_PinDelay_t *pPinDelay = ABC_ALLOC(MiMo_PinDelay_t, 1);
+    pPinDelay->fFromPinOut = 0;
+    pPinDelay->pFromPin = pPinIn;
+    pPinDelay->pNext = pToPinOut->pDelayList;
+    pToPinOut->pDelayList = pPinDelay;
+    return 1;
+  }
+  MiMo_PinOut_t *pPinOut = MiMo_GateFindPinOut(pGate, pFromPinStr);
+  if (pPinOut) {
+    MiMo_PinDelay_t *pPinDelay = ABC_ALLOC(MiMo_PinDelay_t, 1);
+    pPinDelay->fFromPinOut = 1;
+    pPinDelay->pFromPin = pPinOut;
+    pPinDelay->pNext = pToPinOut->pDelayList;
+    pToPinOut->pDelayList = pPinDelay;
+    return 1;
+  }
+  printf("No Input or output pin %s for delay specification not found in gate "
+         "%s\n",
+         pFromPinStr, pGate->pName);
+  return 0;
 }
 
 /**Function*************************************************************
@@ -130,60 +123,53 @@ int MiMo_DelayListAdd(MiMo_Gate_t *pGate, MiMo_PinOut_t * pToPinOut, char *pFrom
                library. Returns 1 on sucess]
 
   Description []
-               
+
   SideEffects []
 
   SeeAlso     []
 
 ***********************************************************************/
 
-int MiMo_LibCheck(MiMo_Library_t *pLib)
-{
-    int fOk = 1;
-    int i, k;
-    MiMo_Gate_t * pGate;
-    MiMo_PinOut_t *pPinOut;
-    MiMo_LibForEachGate(pLib, pGate, i)
-    {
-        Vec_Int_t * vInSeen = Vec_IntStart ( Vec_PtrSize(pGate->pPinIns) );
-        // check that every output has some input 
-        MiMo_GateForEachPinOut(pGate, pPinOut, k)
-        {
-            if ( !pPinOut->pDelayList )
-            {
-                fOk = 0;
-                printf("Output pin %s in gate %s has no input\n", pGate->pName, pPinOut->pName);
-            }
-            MiMo_PinDelay_t * pDelay = pPinOut->pDelayList;
-            while (pDelay)
-            {
-                if (pDelay->Delay < 0)
-                {
-                    fOk = 0;
-                    printf("Gate %s has from pin %s to pin %s a negative delay (%f)\n",
-                            pGate->pName, pPinOut->pName, MiMo_PinDelayInName(pDelay), pDelay->Delay);
-                }
-                if ( ! pDelay->fFromPinOut )
-                    Vec_IntWriteEntry( vInSeen, ((MiMo_PinIn_t*)pDelay->pFromPin)->Id, 1);
-
-                pDelay = pDelay->pNext;
-            }
+int MiMo_LibCheck(MiMo_Library_t *pLib) {
+  int fOk = 1;
+  int i, k;
+  MiMo_Gate_t *pGate;
+  MiMo_PinOut_t *pPinOut;
+  MiMo_LibForEachGate(pLib, pGate, i) {
+    Vec_Int_t *vInSeen = Vec_IntStart(Vec_PtrSize(pGate->pPinIns));
+    // check that every output has some input
+    MiMo_GateForEachPinOut(pGate, pPinOut, k) {
+      if (!pPinOut->pDelayList) {
+        fOk = 0;
+        printf("Output pin %s in gate %s has no input\n", pGate->pName,
+               pPinOut->pName);
+      }
+      MiMo_PinDelay_t *pDelay = pPinOut->pDelayList;
+      while (pDelay) {
+        if (pDelay->Delay < 0) {
+          fOk = 0;
+          printf("Gate %s has from pin %s to pin %s a negative delay (%f)\n",
+                 pGate->pName, pPinOut->pName, MiMo_PinDelayInName(pDelay),
+                 pDelay->Delay);
         }
-        // check that every input is connected to some output
-        int seen;
-        Vec_IntForEachEntry(vInSeen, seen, k)
-            if ( !seen )
-            {
-                fOk = 0;
-                printf("Gate %s has unconnected input pin %s\n", pGate->pName,
-                         ((MiMo_PinIn_t*)Vec_PtrEntry(pGate->pPinIns, k))->pName);
-            }
-        Vec_IntFree(vInSeen);
-        // TODO: check that there is no cyclic output ...
-    }
-    return fOk;
-}
+        if (!pDelay->fFromPinOut)
+          Vec_IntWriteEntry(vInSeen, ((MiMo_PinIn_t *)pDelay->pFromPin)->Id, 1);
 
+        pDelay = pDelay->pNext;
+      }
+    }
+    // check that every input is connected to some output
+    int seen;
+    Vec_IntForEachEntry(vInSeen, seen, k) if (!seen) {
+      fOk = 0;
+      printf("Gate %s has unconnected input pin %s\n", pGate->pName,
+             ((MiMo_PinIn_t *)Vec_PtrEntry(pGate->pPinIns, k))->pName);
+    }
+    Vec_IntFree(vInSeen);
+    // TODO: check that there is no cyclic output ...
+  }
+  return fOk;
+}
 
 /**Function*************************************************************
 
@@ -191,35 +177,31 @@ int MiMo_LibCheck(MiMo_Library_t *pLib)
                input pins of the given output pin]
 
   Description []
-               
+
   SideEffects []
 
   SeeAlso     []
 
 ***********************************************************************/
-void MiMo_PinCalcMaxDelay_rec(MiMo_PinOut_t *pPinOut)
-{
-    if ( pPinOut->MaxDelay >= 0)
-        return;
-    MiMo_PinDelay_t * pDelay = pPinOut->pDelayList;
-    float maxDelay = -1;
-    while ( pDelay )
-    {
-        if (pDelay->fFromPinOut)
-        {
-            MiMo_PinOut_t *pPinOutIn = pDelay->pFromPin;
-            MiMo_PinCalcMaxDelay_rec(pPinOutIn);
-            float delay = pPinOutIn->MaxDelay + pDelay->Delay;
-            if ( maxDelay < delay)
-                maxDelay = delay;
-        } else
-        {
-            if ( maxDelay < pDelay->Delay )
-                maxDelay = pDelay->Delay;
-        }
-        pDelay = pDelay->pNext;
+void MiMo_PinCalcMaxDelay_rec(MiMo_PinOut_t *pPinOut) {
+  if (pPinOut->MaxDelay >= 0)
+    return;
+  MiMo_PinDelay_t *pDelay = pPinOut->pDelayList;
+  float maxDelay = -1;
+  while (pDelay) {
+    if (pDelay->fFromPinOut) {
+      MiMo_PinOut_t *pPinOutIn = pDelay->pFromPin;
+      MiMo_PinCalcMaxDelay_rec(pPinOutIn);
+      float delay = pPinOutIn->MaxDelay + pDelay->Delay;
+      if (maxDelay < delay)
+        maxDelay = delay;
+    } else {
+      if (maxDelay < pDelay->Delay)
+        maxDelay = pDelay->Delay;
     }
-    pPinOut->MaxDelay = maxDelay;
+    pDelay = pDelay->pNext;
+  }
+  pPinOut->MaxDelay = maxDelay;
 }
 
 /**Function*************************************************************
@@ -228,27 +210,77 @@ void MiMo_PinCalcMaxDelay_rec(MiMo_PinOut_t *pPinOut)
                outputs]
 
   Description []
-               
+
   SideEffects []
 
   SeeAlso     []
 
 ***********************************************************************/
-void MiMo_GateCalcMaxDelay(MiMo_Gate_t * pGate)
-{
-    int i;
-    MiMo_PinOut_t *pPinOut;
-    MiMo_GateForEachPinOut(pGate, pPinOut, i)
-        pPinOut->MaxDelay = -1;
-    float maxDelay = -1;
-    MiMo_GateForEachPinOut(pGate, pPinOut, i)
-    {
-        MiMo_PinCalcMaxDelay_rec(pPinOut);
-        if (pPinOut->MaxDelay > maxDelay)
-            maxDelay = pPinOut->MaxDelay;
-    }
-    pGate->MaxDelay = maxDelay;
+void MiMo_GateCalcMaxDelay(MiMo_Gate_t *pGate) {
+  int i;
+  MiMo_PinOut_t *pPinOut;
+  MiMo_GateForEachPinOut(pGate, pPinOut, i) pPinOut->MaxDelay = -1;
+  float maxDelay = -1;
+  MiMo_GateForEachPinOut(pGate, pPinOut, i) {
+    MiMo_PinCalcMaxDelay_rec(pPinOut);
+    if (pPinOut->MaxDelay > maxDelay)
+      maxDelay = pPinOut->MaxDelay;
+  }
+  pGate->MaxDelay = maxDelay;
 }
+/**Function*************************************************************
 
+  Synopsis    [Renames the output pins such that the blif file can be compatible
+with the architecture file of VTR]
 
+  Description [Converting out_a to out[a]. Example: out0 is converted to out[0]]
+
+  SideEffects []
+
+  SeeAlso     []
+
+***********************************************************************/
+char *MiMo_GateOutRenamer(MiMo_PinOut_t *pPinOutOld) {
+  MiMo_PinOut_t pPinOut = *pPinOutOld;
+  char pNewPinName[7] = {};
+  // Basically handling the case for two digit numbers. Caution: To work with 3
+  // digit numbers, you would have to change this function
+  assert(pPinOut.pName[5] != '\[');
+  if (pPinOutOld->pName[4] == '\0')
+    sprintf(pNewPinName, "out[%d]", pPinOutOld->pName[3] - 48);
+  else
+    sprintf(pNewPinName, "out[%d]",
+            (pPinOutOld->pName[3] - 48) * 10 + (pPinOutOld->pName[4] - 48));
+  pPinOut.pName = pNewPinName;
+  return pPinOut.pName;
+}
+/**Function*************************************************************
+
+  Synopsis    [Renames the input pins such that the blif file can be compatible
+with the architecture file of VTR]
+
+  Description [Converting ina[b] to in[a*4 + b]. Example: in1[3] is converted
+to in[7]]
+
+  SideEffects []
+
+  SeeAlso     []
+
+***********************************************************************/
+char *MiMo_GateInRenamer(MiMo_PinIn_t *pPinInOld) {
+  MiMo_PinIn_t pPinIn = *pPinInOld;
+  char pNewPinName[7] = {};
+  // TODO: Need to still implement a better mapping approach to handle NNC2 and
+  // NNC3 together
+  if (pPinInOld->pName[3] == '\[')
+    sprintf(pNewPinName, "in[%d]",
+            (pPinIn.pName[2] - 48) * 4 + (pPinIn.pName[4] - 48));
+  else
+    sprintf(pNewPinName, "in[%d]",
+            ((pPinIn.pName[2] - 48) * 10 + (pPinIn.pName[3] - 48)) * 4 +
+                (pPinIn.pName[5] - 48));
+
+  pPinIn.pName = pNewPinName;
+  return pPinIn.pName;
+}
 ABC_NAMESPACE_IMPL_END
